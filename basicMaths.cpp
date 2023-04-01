5ceaedc27fabde1130fd99fe063a13663d13ef21
0xsagar <sagar.19bct@cmr.edu.in>
2023-04-01 13:11:47 +0530

    day 6

--- basicMaths.cpp
+++ basicMaths.cpp
@@ -113,9 +113,44 @@ void isPrime(int n)
         cout << "false" << endl;
 }
 
+void gcd(int n1, int n2)
+{
+    // int gcd;
+    // for (int i = 1; i <= min(n1, n2); i++)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         gcd = i;
+    //     }
+    // }
+    // cout << gcd;
+
+    // int gcd;
+    // for (int i = min(n1, n2); i >= 1; i--)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         cout << i;
+    //         break;
+    //     }
+    // }
+
+    while (n1 > 0 && n2 > 0)
+    {
+        if (n1 > n2)
+            n1 %= n2;
+        else
+            n2 %= n1;
+    }
+    if (n1 == 0)
+        cout << n2 << endl;
+    else
+        cout << n1 << endl;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    isPrime(n);
+    int n1, n2;
+    cin >> n1 >> n2;
+    gcd(n1, n2);
 }
