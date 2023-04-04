854ac9d2aeba0190c6cb1a3ce4f2ae706e7b96e4
0xsagar <sagar.19bct@cmr.edu.in>
2023-04-04 23:40:36 +0530

    day 8

---
+++ hashing.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Globally hash can store 10 to the power 7 elements
+int hashh[10000000] = {0};
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // Locally hash can store 10 to the power 6 elements
+//     // int hashh[1000000] = {0};
+//     // precompute
+//     for (int i = 0; i < n; i++)
+//     {
+//         hashh[arr[i]] += 1;
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int number;
+//         cin >> number;
+//         cout << hashh[number] << endl;
+//     }
+// }
+
+int main(){
+    
+}
