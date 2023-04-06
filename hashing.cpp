98214dd9e9da7a4fd189e6e622b8ec7cb527b2f1
0xsagar <sagar.19bct@cmr.edu.in>
2023-04-07 00:01:31 +0530

    day 9

--- hashing.cpp
+++ hashing.cpp
@@ -31,6 +31,92 @@ int hashh[10000000] = {0};
 //     }
 // }
 
-int main(){
-    
+// int main()
+// {
+//     string s;
+//     cin >> s;
+//     int hashhh[256] = {0};
+//     // precompute
+//     for (int i = 0; i < s.size(); i++)
+//     {
+//         hashhh[s[i]]++;
+//     }
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         char c;
+//         cin >> c;
+//         cout << hashhh[c] << endl;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     // precompute
+//     unordered_map<int, int> mpp;// most of the time - use unordered_map
+//     //Time complexity of unordered_map is O(1),
+//     //Time complexity of map is O(N)
+//     for (int i = 0; i < n; i++)
+//     {
+//         mpp[arr[i]]++;
+//     }
+
+//     //  iterate in the map
+//     for (auto it : mpp)
+//     {
+//         cout << it.first << "->" << it.second << endl;
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int number;
+//         cin >> number;
+//         // fetch
+//         cout << mpp[number] << endl;
+//     }
+//     return 0;
+// }
+
+// Question: Find the highest and lowest frequency element in the array
+int main()
+{
+    int n, highest = INT_MIN, lowest = INT_MAX, h_Ele, l_Ele;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // precompute
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+    for (auto it : mpp)
+    {
+        if (it.second >= highest)
+        {
+            highest = it.second;
+            h_Ele = it.first;
+        }
+        if (it.second <= lowest)
+        {
+            lowest = it.second;
+            l_Ele = it.first;
+        }
+    }
+    cout << h_Ele << " and " << l_Ele << endl;
+    return 0;
 }
