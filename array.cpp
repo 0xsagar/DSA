7acbad22b459d15b3f8afb61b2c20d0a427d8008
0xsagar <sagar.19bct@cmr.edu.in>
2023-04-15 01:39:58 +0530

    day 13

--- array.cpp
+++ array.cpp
@@ -59,12 +59,12 @@ void reverseArray(int demo[], int n)
 // Question: Swap alternate elements of an array
 void swapAlternate(int demo[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i += 2)
     {
         int temp = demo[i + 1];
         demo[i + 1] = demo[i];
         demo[i] = temp;
-        i++;
+        // i++;
     }
     for (int i = 0; i < n; i++)
     {
@@ -72,7 +72,7 @@ void swapAlternate(int demo[], int n)
     }
 }
 
-// Question: Find unique element in an array
+// Question: Find unique element in an array using map
 void uniqueElement(int demo[], int n)
 {
     // pre-compute
@@ -89,6 +89,17 @@ void uniqueElement(int demo[], int n)
             cout << it.first << " ";
         }
     }
+}
+
+// Question: Find unique element in an array
+void uniqueElement2(int demo[], int n)
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ demo[i];
+    }
+    cout << ans << " ";
 }
 
 // Question: Find duplicate elements in an array
@@ -122,6 +133,34 @@ void arrayIntersection(int demo[], int demo2[], int n)
                 cout << demo[i] << " ";
             }
         }
+    }
+}
+
+// Question: Find the Array Intersection for sorted Arrays (better complexity)
+void arrayIntersection2(int demo[], int demo2[], int n, int m)
+{
+    int i = 0, j = 0;
+    vector<int> ans;
+    while (i < n && j < m)
+    {
+        if (demo[i] == demo2[j])
+        {
+            ans.push_back(demo[i]);
+            i++;
+            j++;
+        }
+        else if (demo[i] < demo2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    for (auto it : ans)
+    {
+        cout << it << " ";
     }
 }
 
@@ -166,6 +205,7 @@ bool checkSortedArray(int demo[], int n)
     return 1;
 }
 
+// Question: Check if the array is sorted and check for rotations also
 bool checkSortedArrayAndRotation(vector<int> &nums)
 {
     int count = 0;
@@ -181,6 +221,115 @@ bool checkSortedArrayAndRotation(vector<int> &nums)
     return count <= 1;
 }
 
+// Question: Pair Sum
+void pairSum(int demo[], int n, int s)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (demo[i] + demo[j] == s)
+            {
+                cout << min(demo[i], demo[j]) << " " << max(demo[i], demo[j]) << endl;
+            }
+        }
+    }
+}
+
+// Question: Triplet Sum
+void tripletSum(int demo[], int n, int s)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (demo[i] + demo[j] + demo[k] == s)
+                {
+                    cout << demo[i] << " " << demo[j] << " " << demo[k] << endl;
+                }
+            }
+        }
+    }
+}
+
+// Question: Sort 0 & 1
+void sortZeroOne(int arr0[], int n)
+{
+    int p1 = 0, p2 = n - 1;
+    while (p1 < p2)
+    {
+        if (arr0[p1] == 0 && p1 < p2)
+        {
+            p1++;
+        }
+        if (arr0[p2] == 1 && p1 < p2)
+        {
+            p2--;
+        }
+        if (arr0[p1] > arr0[p2] && p1 < p2)
+        {
+            swap(arr0[p1], arr0[p2]);
+            p1++;
+            p2--;
+        }
+    }
+    for (int x = 0; x < n; x++)
+    {
+        cout << arr0[x] << " ";
+    }
+}
+
+// Question: Sort 0, 1 & 2
+void sortZeroOneTwo(int arr[], int n)
+{
+    // One way to solve this problem is to use the Dutch National Flag algorithm
+    int low = 0, mid = 0, high = n - 1;
+    // while (mid <= high)
+    // {
+    //     if (arr[mid] == 0)
+    //     {
+    //         swap(arr[low], arr[mid]);
+    //         low++;
+    //         mid++;
+    //     }
+    //     else if (arr[mid] == 1)
+    //     {
+    //         mid++;
+    //     }
+    //     else
+    //     {
+    //         swap(arr[mid], arr[high]);
+    //         high--;
+    //     }
+    // }
+
+    while (mid <= high)
+    {
+        switch (arr[mid])
+        {
+        case 0:
+            swap(arr[low++], arr[mid++]);
+            // low++;
+            // mid++;
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(arr[mid], arr[high--]);
+            // high--;
+            break;
+        }
+    }
+
+    for (int x = 0; x < n; x++)
+    {
+        cout << arr[x] << " ";
+    }
+}
+
 int main()
 {
     int n, minimum = INT_MAX, maximum = INT_MIN, key;
@@ -193,8 +342,8 @@ int main()
     // minMaxArray(arr, n, minimum, maximum);
     // arraySum(arr, n);
 
-    int demo[5] = {1, 9, 2, 3, 4};
-    int demo2[5] = {1, 8, 3, 4, 6};
+    int demo[5] = {1, 2, 3, 4, 5};
+    int demo2[5] = {1, 2, 3, 4, 6};
     // cin >> key;
     // cout << linearSearch(demo, 10, key);
     //  bool found = linearSearch(demo, 10, key);
@@ -209,17 +358,31 @@ int main()
 
     // reverseArray(demo, 10);
 
-    // swapAlternate(demo, 10);
+    // swapAlternate(demo, 5);
 
-    // uniqueElement(demo, 10);
+    // uniqueElement(demo, 5);
+
+    // uniqueElement2(demo, 5);
 
     // duplicateElement(demo, 10);
 
     // arrayIntersection(demo, demo2, 5);
+
+    // arrayIntersection2(demo, demo2, 5, 5);
 
     // secondSmallestandLargestElement(demo, 5);
 
     // cout << checkSortedArray(demo, 5);
     // vector<int> nums = {5, 4, 7, 1, 2, 3};
     // cout << checkSortedArrayAndRotation(nums);
+
+    // pairSum(demo, 5, 5);
+
+    // tripletSum(demo, 5, 10);
+
+    // int arr0[9] = {1, 1, 0, 1, 0, 0, 0, 1, 0};
+    // sortZeroOne(arr0, 9);
+
+    // int arr[9] = {1, 2, 0, 1, 2, 0, 0, 1, 0};
+    // sortZeroOneTwo(arr, 9);
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,4 @@
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < n; i++)
     // {
-    //     cout << demo[i] << " ";
+    //     cout << arr0[i] << " ";
     // }
