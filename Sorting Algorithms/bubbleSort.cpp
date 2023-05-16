e1922c558374cb90eac906da55c2f7f08835c70c
0xsagar <sagar.19bct@cmr.edu.in>
2023-05-16 16:19:54 +0530

    little adjustment in bubble sort

--- Sorting Algorithms/bubbleSort.cpp
+++ Sorting Algorithms/bubbleSort.cpp
@@ -1,26 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// void bubbleSort(int arr[], int n)
+// {
+//     for (int i = n - 1; i >= 0; i--)
+//     {
+//         int didSwap = 0;
+//         for (int j = 0; j <= i - 1; j++)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 int temp = arr[j + 1];
+//                 arr[j + 1] = arr[j];
+//                 arr[j] = temp;
+//                 didSwap = 1;
+//             }
+//         }
+//         if (didSwap == 0)
+//         {
+//             break;
+//         }
+//     }
+// }
+
 void bubbleSort(int arr[], int n)
 {
-    for (int i = n - 1; i >= 0; i--)
+    int totalSwaps = 0;
+    // for rounds 1 to n - 1
+    for (int i = 1; i < n; i++)
     {
-        int didSwap = 0;
-        for (int j = 0; j <= i - 1; j++)
+
+        bool didSwap = false;
+        // process elements till (n - 1)th index
+        for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
-                didSwap = 1;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                didSwap = true;
+                totalSwaps++;
             }
         }
-        if (didSwap == 0)
+        if (didSwap == false)
         {
             break;
         }
     }
+    cout << "Total Number of Swaps: " << totalSwaps << endl;
 }
 
 // Best Case Time Complexity = O(n)
