67f96144075c91aa03758742ab6a1f76964148ec
0xsagar <sagar.19bct@cmr.edu.in>
2023-06-30 00:21:00 +0530

    did 5 questions from supreme batch dsa

--- Supreme Batch/week_3/hw.cpp
+++ Supreme Batch/week_3/hw.cpp
@@ -96,25 +96,23 @@ void findDuplicate(int arr[], int n)
     // {
     //     if (it.second >= 2)
     //     {
-    //         cout << it.first << " ";
+    //         cout << it.first << endl;
     //     }
     // }
 
-    // Occurrance
-    // int occ = 0;
+    // Occurrance (for all elements repeating only 2 times)
     // for (int i = 0; i < n; i++)
     // {
     //     for (int j = i + 1; j < n; j++)
     //     {
     //         if (arr[i] == arr[j])
     //         {
-    //             occ = arr[i];
+    //             cout << arr[i] << endl;
     //         }
     //     }
     // }
-    // cout << occ;
 
-    // Positioning methods
+    // Positioning methods (only for 1 element repeating twice in the array)
     // while (arr[0] != arr[arr[0]])
     // {
     //     swap(arr[0], arr[arr[0]]);
@@ -122,7 +120,147 @@ void findDuplicate(int arr[], int n)
     // cout << arr[0];
 }
 
-// Question
+// Question: Find the missing element of the array
+void findMissing(int arr[], int n)
+{
+    int missing = 0;
+    int largest = INT_MIN, smallest = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    cout << "Largest element is: " << largest << endl;
+    int total_sum = (largest * (largest + 1)) / 2;
+    cout << "Total sum of the numbers: " << total_sum << endl;
+    for (int i = 0; i < n; i++)
+    {
+        total_sum -= arr[i];
+    }
+    cout << "The missing element is: " << total_sum;
+}
+
+// Question: Find the first repeating element
+int findFirstRepeating(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // cout << arr[i] << " " << arr[j] << endl;
+            if (arr[i] == arr[j])
+            {
+                cout << arr[j] << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No repeating element found!" << endl;
+    return 0;
+}
+
+// Question: Find common element in 3 arrays
+void findCommon(int arr1[], int arr2[], int arr3[], int n)
+{
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr1[i]]++;
+        mpp[arr2[i]]++;
+        mpp[arr3[i]]++;
+    }
+    for (auto it : mpp)
+    {
+        if (it.second == 3)
+        {
+            cout << it.first << endl;
+        }
+    }
+}
+
+// Question: Print the matrix in wave form
+void waveMatrix(vector<vector<int>> v)
+{
+    int rows = v.size();
+    int cols = v[0].size();
+    for (int i = 0; i < cols; i++)
+    {
+        if ((i & 1) == 0)
+        {
+            // even number of rows == top to bottom
+            for (int j = 0; j < rows; j++)
+            {
+                cout << v[j][i] << " ";
+            }
+        }
+        else
+        {
+            // odd number of rows == bottom to top
+            for (int j = rows - 1; j >= 0; j--)
+            {
+                cout << v[j][i] << " ";
+            }
+        }
+    }
+}
+
+// Question: Print the matrix in spiral form
+void spiralMatrix(vector<vector<int>> &v)
+{
+    vector<int> ans;
+    int rows = v.size();
+    int cols = v[0].size();
+    int total_elements = rows * cols;
+
+    int startingRow = 0;
+    int endingCol = cols - 1;
+    int endingRow = rows - 1;
+    int startingCol = 0;
+
+    int count = 0;
+
+    while (count < total_elements)
+    {
+        // print startingRow
+        for (int i = startingCol; i <= endingCol && count < total_elements; i++)
+        {
+            ans.push_back(v[startingRow][i]);
+            count++;
+        }
+        startingRow++;
+
+        // print endingCol
+        for (int i = startingRow; i <= endingRow && count < total_elements; i++)
+        {
+            ans.push_back(v[i][endingCol]);
+            count++;
+        }
+        endingCol--;
+
+        // print endingRow
+        for (int i = endingCol; i >= startingCol && count < total_elements; i--)
+        {
+            ans.push_back(v[endingRow][i]);
+            count++;
+        }
+        endingRow--;
+
+        // print startingCol
+        for (int i = endingRow; i >= startingRow && count < total_elements; i--)
+        {
+            ans.push_back(v[i][startingCol]);
+            count++;
+        }
+        startingCol++;
+    }
+    // printing the new vector with spiral sequence
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
 
 int main()
 {
@@ -134,7 +272,27 @@ int main()
     // int size = 6;
     // sortNegative(arr, size);
 
-    // int arr[7] = {4, 1, 2, 3, 5, 6, 6};
+    // int arr[7] = {4, 1, 2, 1, 5, 5, 6};
     // int size = 7;
     // findDuplicate(arr, size);
+
+    // int arr[7] = {1, 2, 3, 4, 6, 5, 8};
+    // int size = 7;
+    // findMissing(arr, size);
+
+    // int arr[6] = {1, 2, 3, 4, 3, 5};
+    // int size = 6;
+    // findFirstRepeating(arr, size);
+
+    // int arr1[5] = {1, 2, 3, 4, 5};
+    // int arr2[5] = {2, 3, 6, 7, 8};
+    // int arr3[5] = {1, 2, 3, 7, 8};
+    // int size = 5;
+    // findCommon(arr1, arr2, arr3, size);
+
+    // vector<vector<int>> v{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    // waveMatrix(v);
+
+    // vector<vector<int>> v{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    // spiralMatrix(v);
 }
